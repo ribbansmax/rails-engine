5584b2d2efdd30139237bd3ee542a61454375e25
Max Ribbans <ribbans.max@gmail.com>
2021-02-14 12:06:55 -0500

    Factories setup

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
+    name { Faker::Vehicle.make_and_model }
+    description { Faker::Lorem.sentence }
     unit_price { 1.5 }
     merchant_id { nil }
   end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :merchant do
-    name { "MyString" }
+    name { Faker::Books::Dune.character }
   end
 end
