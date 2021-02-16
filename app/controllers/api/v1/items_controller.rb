76e650dda51dee68600095b10edc7f7c87962ed9
Max Ribbans <ribbans.max@gmail.com>
2021-02-16 15:12:12 -0500

    Refactor for json_api gem

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: ItemSerializer.format_items(params[:page], params[:per_page])
+    render json: ItemSerializer.new(items)
   end
   
   def show
-    render json: ItemSerializer.format_item(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
@@ -15,5 +15,13 @@ class Api::V1::ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def items
+    page = params[:page]
+    per_page = params[:per_page]
+    page = 1 if page.nil?
+    per_page = 20 if per_page.nil?
+    Item.all[((page.to_i - 1) * per_page.to_i)..(page.to_i * per_page.to_i - 1)] || []
   end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,13 +1,23 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.format_merchants(params[:page], params[:per_page])
+    render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    render json: MerchantSerializer.format_merchant(params[:id])
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 
   def items_index
-    render json: MerchantSerializer.format_items(params[:id])
+    render json: ItemSerializer.new(Merchant.find(params[:id]).items)
+  end
+
+  private
+
+  def merchants
+    page = params[:page]
+    per_page = params[:per_page]
+    page = 1 if page.nil?
+    per_page = 20 if per_page.nil?
+    Merchant.all[((page.to_i - 1) * per_page.to_i)..(page.to_i * per_page.to_i - 1)] || []
   end
 end
