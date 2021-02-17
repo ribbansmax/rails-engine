82c807bee9b4383d2ed1fd946d8fdd23881af276
Max Ribbans <ribbans.max@gmail.com>
2021-02-17 10:29:07 -0500

    tests for item search

--- spec/requests/api/v1/items/search_items_spec.rb
+++ spec/requests/api/v1/items/search_items_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'Item find_all' do
   it 'should return all items that match' do
-    item = create(:item, name: 'test')
-    item2 = create(:item, name: 'test1')
-    item3 = create(:item, name: 'test2')
+    item = create(:item, name: 'test', unit_price: 100)
+    item2 = create(:item, name: 'test1', unit_price: 10)
+    item3 = create(:item, name: 'test2', unit_price: 1)
 
     get "/api/v1/items/find_all?name=test"
 
@@ -18,5 +18,27 @@ describe 'Item find_all' do
     item_info = JSON.parse(response.body, symbolize_names: true)
 
     expect(item_info[:data].first[:attributes][:name]).to eq(item2.name)
+
+    get "/api/v1/items/find_all?min_price=10"
+
+    expect(response).to be_successful
+    item_info = JSON.parse(response.body, symbolize_names: true)
+    expect(item_info[:data].length).to eq(2)
+
+    get "/api/v1/items/find_all?max_price=10"
+
+    expect(response).to be_successful
+    item_info = JSON.parse(response.body, symbolize_names: true)
+    expect(item_info[:data].length).to eq(2)
+
+    get "/api/v1/items/find_all?min_price=10&max_price=50"
+
+    expect(response).to be_successful
+    item_info = JSON.parse(response.body, symbolize_names: true)
+    expect(item_info[:data].length).to eq(1)
+
+    get "/api/v1/items/find_all?min_price=10&name=test"
+
+    expect(response).not_to be_successful
   end
 end
