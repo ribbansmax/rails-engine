b81f305f2ba8e036962f6a07c0316b7ca4d61232
Max Ribbans <ribbans.max@gmail.com>
2021-02-18 12:47:03 -0500

    Add test for merchant total revenue

--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -23,4 +23,25 @@ describe "Merchant API" do
     expect(attributes).to have_key(:name)
     expect(attributes[:name]).to be_a(String)
   end
+
+  it "sends the merchants revenue" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    items = create_list(:item, 11, merchant: merchant)
+    invoice = create(:invoice, merchant: merchant, customer: customer)
+    transaction = create(:transaction, invoice: invoice, result: "success")
+    items.each_with_index do |item, index|
+      create(:invoice_item, item: item, invoice: invoice, quantity: (20 - index))
+    end
+
+    get "/api/v1/revenue/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+
+    merchant_info = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_info[:data].length).to eq(1)
+
+    expect(merchant_info[:data][:attributes][:name]).to eq(merchant.name)
+  end
 end
