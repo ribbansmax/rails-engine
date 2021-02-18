ff1202af6676b8638361e123ca751bac07862c14
Max Ribbans <ribbans.max@gmail.com>
2021-02-18 17:14:54 -0500

    Add model tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can sort by potential_revenue" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    invoices = create_list(:invoice, 11, customer: customer, merchant: merchant, status: 'unshipped')
+    item = create(:item, merchant: merchant)
+    invoices.each_with_index do |invoice, index|
+      create(:invoice_item, item: item, invoice: invoice, quantity: (index + 1))
+      create(:transaction, invoice: invoice, result: "success")
+    end
+
+    expect(Invoice.by_potential_revenue(1).first.potential_revenue).to eq((11 * 1.5))
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,6 +9,39 @@ RSpec.describe Merchant, type: :model do
     expect(Merchant.search('test')).to eq(merchant)
     expect(Merchant.search('1')).to eq(merchant2)
     expect(Merchant.search('other words')).to eq({})
+  end
 
+  it "can sort by revenue" do
+    merchant = create(:merchant)
+    merchant2 = create(:merchant)
+    customer = create(:customer)
+    invoices = create_list(:invoice, 11, customer: customer, merchant: merchant, status: 'shipped')
+    invoices2 = create_list(:invoice, 11, customer: customer, merchant: merchant2, status: 'shipped')
+    item = create(:item, merchant: merchant)
+    item2 = create(:item, merchant: merchant2)
+    invoices.each_with_index do |invoice, index|
+      create(:invoice_item, item: item, invoice: invoice, quantity: (index + 1))
+      create(:transaction, invoice: invoice, result: "success")
+    end
+    invoices2.each_with_index do |invoice, index|
+      create(:invoice_item, item: item, invoice: invoice, quantity: (index + 2))
+      create(:transaction, invoice: invoice, result: "success")
+    end
+
+    expect(Merchant.by_revenue(1).first).to eq(merchant2)
+    expect(Merchant.by_revenue(1).first.revenue).to eq(115.5)
+  end
+
+  it "can calculate total revenue" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    invoices = create_list(:invoice, 11, customer: customer, merchant: merchant, status: 'shipped')
+    item = create(:item, merchant: merchant)
+    invoices.each_with_index do |invoice, index|
+      create(:invoice_item, item: item, invoice: invoice, quantity: (index + 1))
+      create(:transaction, invoice: invoice, result: "success")
+    end
+
+    expect(merchant.total_revenue.revenue).to eq(99.0)
   end
 end
