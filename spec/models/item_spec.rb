ee9e483d5e546586cd483b28404ed487308375cb
Max Ribbans <ribbans.max@gmail.com>
2021-02-18 17:27:09 -0500

    Add model tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,5 +12,20 @@ RSpec.describe Item, type: :model do
       expect(Item.name_search("cRosB")).to eq([item])
       expect(Item.name_search("Christmas Album")).to eq([item, item2])
     end
+
+    it "can check params are valid" do
+      params = {}
+      params[:min_price] = 1
+      params2 = {}
+      params2[:max_price] = 10
+      params2[:name] = "Bing Crosby"
+
+      expect(Item.params_check(params)).to be true
+      expect(Item.params_check(params2)).to be false
+
+      params[:max_price] = 2
+
+      expect(Item.params_check(params)).to be true
+    end
   end
 end
